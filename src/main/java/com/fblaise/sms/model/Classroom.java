e8bf257ef54c8441383575db49ad058d944fdeef
Blaise Feukouo <blaise_feukjouo@yahoo.fr>
2019-01-17 11:49:16 +0100

    jj

--- src/main/java/com/fblaise/sms/model/Classroom.java
+++ src/main/java/com/fblaise/sms/model/Classroom.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "classroom")
