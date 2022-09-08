17b463f80fdeae57e0142739427cd626513afa30
bfeukouo <blaise.feukouo@soprasteria.com>
2022-09-08 11:27:04 +0200

    Role students added

--- src/main/java/com/fblaise/sms/AttributesNames.java
+++ src/main/java/com/fblaise/sms/AttributesNames.java
@@ -3,5 +3,6 @@ package com.fblaise.sms;
 public interface AttributesNames {
 	public final String Role_Admin = "ADMIN";
 	public final String Role_Teacher = "TEACHER";
+	public final String Role_Student= "STUDENT";
 
 }
